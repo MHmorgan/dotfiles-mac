5a595a6df453ea2c6ce908869ee77702759244d9
Magnus Aa. Hirth <magnus.hirth@gmail.com>
2023-07-24 15:04:14 +0200

    Update doctor

--- .config/zsh/misc/doctor.zsh
+++ .config/zsh/misc/doctor.zsh
@@ -1,8 +1,14 @@
 
 #DOC> doctor :: Do some sanity checks of the system :: ESSENTIALS
 function doctor {
+
+    # PATH
+
     for P in ${(s.:.)PATH}; do
         test -d $P || echo "PATH: $P not found"
     done
+
+    echo "GIT: Updating $#GIT_REPOS git repos."
+    echo "GOTO: There are $#GOTO_DIRS goto directories."
 }
 
