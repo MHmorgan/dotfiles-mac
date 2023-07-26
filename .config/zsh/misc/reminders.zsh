2197b51359a60e28a8424f5e4bac7669a1b6305e
Magnus Aa. Hirth <magnus.hirth@gmail.com>
2023-07-26 20:32:44 +0200

    Fix reminders function

--- .config/zsh/misc/reminders.zsh
+++ .config/zsh/misc/reminders.zsh
@@ -5,6 +5,6 @@ function reminders {
     find -E . -type f -regex ".*\.(go|java|kt|rs)$" | xargs rg '//.*(@\w+|\b(TODO|FIXME|BUG)\b)'
 
     # Languages with # comments
-    find -E . -type f -regex ".*\.(pl|py|zsh)$" | xargs rg '#.*(@\w+|\(TODO|FIXME|BUG)\b)'
+    find -E . -type f -regex ".*\.(pl|py|zsh)$" | xargs rg '#.*(@\w+|\b(TODO|FIXME|BUG)\b)'
 }
 
