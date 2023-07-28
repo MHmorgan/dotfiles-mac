c0f04136d6233ef207399621dcff355b79c162b9
Magnus Aa. Hirth <magnus.hirth@gmail.com>
2023-07-28 17:34:46 +0200

    Update reminders function

--- .config/zsh/misc/reminders.zsh
+++ .config/zsh/misc/reminders.zsh
@@ -1,10 +1,12 @@
 
 #DOC> reminders :: Look for reminders in comments of source files under cwd :: INFO
 function reminders {
+    local RE='(@\w+|\b(TODO|FIXME|BUG)\b)'
+
     # Languages with // comments
-    find -E . -type f -regex ".*\.(go|java|kt|rs)$" | xargs rg '//.*(@\w+|\b(TODO|FIXME|BUG)\b)'
+    find -E . -type f -regex ".*\.(go|java|kt|rs)$" | xargs rg "//.*$RE"
 
     # Languages with # comments
-    find -E . -type f -regex ".*\.(pl|py|zsh)$" | xargs rg '#.*(@\w+|\b(TODO|FIXME|BUG)\b)'
+    find -E . -type f -regex ".*\.(pl|py|zsh)$" | xargs rg "#.*$RE"
 }
 
