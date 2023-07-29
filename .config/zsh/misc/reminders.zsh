d2bce9626d1c5f8416104e6bbf19d4ec3ed86892
Magnus Aa. Hirth <magnus.hirth@gmail.com>
2023-07-29 15:57:15 +0200

    Support goto directories for reminders

--- .config/zsh/misc/reminders.zsh
+++ .config/zsh/misc/reminders.zsh
@@ -1,12 +1,30 @@
 
-#DOC> reminders :: Look for reminders in comments of source files under cwd :: INFO
+#DOC> reminders [NAME] :: Look for reminders in comments of source files :: INFO
 function reminders {
     local RE='(@\w+|\b(TODO|FIXME|BUG)\b)'
+    local DIR=${1:-.}
+
+    if (( $# > 0 )); then
+        local TMP
+        for TMP in $GOTO_DIRS; do
+            if [[ $1 == ${TMP##*/} ]]; then
+                DIR=$TMP
+                break
+            fi
+        done
+    fi
+
+    test -d $DIR || { err "not a directory: $DIR"; return 1 }
 
     # Languages with // comments
-    find -E . -type f -regex ".*\.(go|java|kt|rs)$" | xargs rg "//.*$RE"
+    find -E $DIR -type f -regex ".*\.(go|java|kt|rs)$" | xargs rg "//.*$RE"
 
     # Languages with # comments
-    find -E . -type f -regex ".*\.(pl|py|zsh)$" | xargs rg "#.*$RE"
+    find -E $DIR -type f -regex ".*\.(pl|py|zsh)$" | xargs rg "#.*$RE"
 }
+
+export __REMINDERS_HELP='usage: reminders [NAME]
+
+Look for reminders in comments of source files.
+'
 
